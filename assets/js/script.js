eabf3ff8f3b705209503287b9fc41a1716995df7
Katia Kaori Kaminishikawahara <kkaori146@gmail.com>
2021-06-07 22:43:25 +0900

    Função Cancelar Implementada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
+let modalQt = 1;
+
 const c = (el) => document.querySelector(el);
-const css = (el) => document.querySelectorAll(el);
+const cs = (el) => document.querySelectorAll(el);
+
+//LISTAGEM DAS PIZZAS
 
 pizzaJson.map((item, index) => {
 let pizzaItem = c('.models .pizza-item').cloneNode(true);
@@ -16,21 +20,79 @@ pizzaItem.querySelector('a').addEventListener('click', (e)=>{
 e.preventDefault();
 
 let key = e.target.closest('.pizza-item').getAttribute('data-key');
+modalQt = 1;
 
 c('.pizzaBig img').src = pizzaJson[key].img;
-
 c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
 c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+c('.pizzaInfo--size.selected').classList.remove('selected');
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+    if(sizeIndex == 2) {
+        size.classList.add('selected');
+    }
+    size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+});
+
+c('.pizzaInfo--qt').innerHTML = modalQt;
+
 
 c('.pizzaWindowArea').style.opacity = 0;
 
 c('.pizzaWindowArea').style.display = 'flex';
 setTimeout(()=>{
     c('.pizzaWindowArea').style.opacity = 1;
-},200);
+}, 200);
 
 });
 
 c('.pizza-area').append( pizzaItem );
 
 });
+
+//EVENTOS DO MODAL
+
+function closeModal() {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none';
+    },500);
+}
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
