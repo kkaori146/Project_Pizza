29fcf128b3127a4cc56b2dd94bc4f1b1dc407bd5
Katia Kaori Kaminishikawahara <kkaori146@gmail.com>
2021-06-07 23:11:27 +0900

    Quantidade de Pizzas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
+let cart = [];
 let modalQt = 1;
+let modalKey = 0;
 
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
@@ -21,6 +23,7 @@ e.preventDefault();
 
 let key = e.target.closest('.pizza-item').getAttribute('data-key');
 modalQt = 1;
+modalKey = key;
 
 c('.pizzaBig img').src = pizzaJson[key].img;
 c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -72,8 +75,23 @@ c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
 modalQt++;
 c('.pizzaInfo--qt').innerHTML = modalQt;
 });
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+    size.addEventListener('click', (e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+   
+let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
 
-
+cart.push({
+id:pizzaJson[modalKey].id,
+size,
+qt:modalQt
+});
+closeModal();
+});
 
 
 
