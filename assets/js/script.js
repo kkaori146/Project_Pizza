1246a2a9ebe3ad6fff3a4568592a84156d6292f5
Katia Kaori Kaminishikawahara <kkaori146@gmail.com>
2021-06-08 00:19:44 +0900

    Trabalhando com o tamanho das pizzas

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,11 +85,21 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
    
 let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
 
+
+let identifier = pizzaJson[modalKey].id+'@'+size;
+
+let key = cart.findIndex((item)=>item.identifier == identifier);
+
+if(key > -1) {
+cart[key].qt += modalQt;
+} else {
 cart.push({
+    identifier,
 id:pizzaJson[modalKey].id,
 size,
 qt:modalQt
 });
+}
 closeModal();
 });
 
